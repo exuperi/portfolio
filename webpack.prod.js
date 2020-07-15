10f64c7d7017c5bb063cbfed3c3cfec2cdb925ed
Siarhei Drozd <s.drozd@cheil.com>
2020-07-15 16:43:19 +0200

    favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,6 +24,7 @@ module.exports = merge(common, {
 					collapseWhitespace: true,
 					removeComments: true,
 				},
+				favicon: "src/assets/favicon-32x32.png",
 			}),
 		],
 	},
@@ -31,7 +32,6 @@ module.exports = merge(common, {
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({
 			filename: "[name].[contentHash].css",
-			favicon: "src/assets/favicon-32x32.png"
 		}),
 	],
 	module: {
